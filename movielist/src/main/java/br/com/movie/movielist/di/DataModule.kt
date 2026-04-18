ab224eafda9c5055b329f405f8c4521d2f477057
Mychelle Rocha Mendes <mychellerochamendes@MacBook-Air-de-Mychelle.local>
2026-04-18 20:18:17 -0300

    refactor: injeção do CoroutineDispatcher no repository para melhor testabilidade

--- movielist/src/main/java/br/com/movie/movielist/di/DataModule.kt
+++ movielist/src/main/java/br/com/movie/movielist/di/DataModule.kt
@@ -5,13 +5,17 @@ import br.com.movie.movielist.data.mapper.MovieMapperImpl
 import br.com.movie.movielist.data.repository.MovieRepositoryImpl
 import br.com.movie.movielist.domain.repository.MovieRepository
 import br.com.movie.movielist.domain.usecase.GetMyListsUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val movieListModule = module {
 
+    single<CoroutineDispatcher> { Dispatchers.IO }
+
     factory<MovieMapper> { MovieMapperImpl() }
 
-    single<MovieRepository> { MovieRepositoryImpl(service = get(), mapper = get()) }
+    single<MovieRepository> { MovieRepositoryImpl(service = get(), mapper = get(), ioDispatcher = get() ) }
 
     factory { GetMyListsUseCase(get()) }
 }
